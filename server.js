c113f8569318a836f8cfd3a6af8ab76f78cf869d
ludralph <raphaelumoh@gmail.com>
2020-02-08 09:11:57 +0100

    setup middleware for the bootcamp route

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bootcamps = require('./routes/bootcamps');
 
 // load config vars
 
 dotenv.config({ path: './config/config.env'});
 
 const app = express();
+
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
