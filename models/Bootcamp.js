d31923c2a6d40bfb6a4c047fd38b023be5656de0
ludralph <raphaelumoh@gmail.com>
2020-02-12 16:29:22 +0100

    implement slug field using mongoose hooks

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -95,5 +96,12 @@ const BootcampSchema = new mongoose.Schema({
         default: Date.now
       },
 });
+
+// createbootcamp slug from the name
+
+BootcampSchema.pre('save', function() {
+  this.slug = slugify(this.name, { lower: true });
+  next()
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
