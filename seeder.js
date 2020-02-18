2c5ee9520f3c006eb1e78f15d02285238511af48
ludralph <raphaelumoh@gmail.com>
2020-02-18 14:38:25 +0100

    add database seeder to import and delete data

---
+++ seeder.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env'});
+
+// Load models
+const Bootcamp = require('./models/Bootcamp');
+ 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Read JSON files
+
+const bootcamp = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json}`), 'utf-8');
+
+
+// import into DB
+const importData = async () => {
+    try{
+        await Bootcamp.create(bootcamps);
+        console.log('Data Imported...');
+        process.exit()
+    }
+    catch(err){
+        console.log(err);
+        
+    }
+}
+
+const deleteData = async () => {
+    try{
+        await Bootcamp.deleteMany();
+        console.log('Data destroyed...');
+        process.exit()
+    }
+    catch(err){
+        console.log(err);
+        
+    }
+}
+
+if (process.argv[2] === '-i'){
+    importData();
+}
+else if( process.argv[2] === '-d'){
+    deleteData();
+}
